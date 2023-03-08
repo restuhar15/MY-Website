d06756b5d27468fa414022555b8edbd3d357c59d
restuhar15 <127292667+restuhar15@users.noreply.github.com>
2023-03-08 15:32:57 +0700

    Add files via upload

---
+++ program_memberi_semangat_nitizen.pas
@@ -0,0 +1,19 @@
+program program_memberi_semangat_nitizen;
+uses crt;
+var greetings, nama, umur, ucapan1, ucapan2, nasihat: string;
+begin
+clrscr;
+  writeln('Selamat Sore,Manusia-Manusia ^_^ ');
+  readln(greetings);
+  writeln('Siapa nama anda ?');
+  readln(nama);
+  writeln('Berapa umur anda ?');
+  readln(umur);
+  writeln('Apa masalah terberat anda ? (boleh diceritakan atau tidak)' );
+  readln(ucapan1);
+  writeln('Apakah anda bahagia ?' );
+  readln(ucapan2);
+  writeln(' "Apapun masalah anda,bahagia atau tidak,hidup akan terus berjalan,percayalah semuanya akan baik-baik saja,Tuhan selalu bersamamu,kamu manusia kuat,semangattt hhe');
+  readln(nasihat);
+  readln;
+end.
